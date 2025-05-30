f36b7dba630a39b7a912e17901719b8433c99b5c
swallow-234 <s0955502379@gmail.com>
2025-05-30 13:37:03 +0800

    Delete ~青雲間之飛燕~ directory

--- ~青雲間之飛燕~/《聖光教教主》/分頁.js
+++
@@ -1,8 +0,0 @@
-    function showTab(index) {
-      const tabs = document.querySelectorAll('.tab');
-      const contents = document.querySelectorAll('.tab-content');
-      tabs.forEach((tab, i) => {
-        tab.classList.toggle('active', i === index);
-        contents[i].classList.toggle('active', i === index);
-      });
-    }
