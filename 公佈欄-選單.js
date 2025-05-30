d15476d6de6037909aba851c1cbc06b742e70f07
swallow-234 <s0955502379@gmail.com>
2025-05-30 14:08:19 +0800

    Update 公佈欄-選單.js

--- 公佈欄-選單.js
+++ 公佈欄-選單.js
@@ -1,7 +1,7 @@
 AA ='<a href="https://swallow-234.github.io/swallow_234/%E5%85%AC%E4%BD%88%E6%AC%84.html">首頁</a>\
   	<ul >\
 		<p style="text-align: center;">《青丘之燕》</p><hr>\
-        <li><a href="https://swallow-234.github.io/swallow_234/%E3%80%8A%E8%81%96%E5%85%89%E6%95%99%E6%95%99%E4%B8%BB%E3%80%8B/%E3%80%8A%E8%81%96%E5%85%89%E6%95%99%E6%95%99%E4%B8%BB%E3%80%8B.html">《聖光教教主》</a></li>\
+        <li><a href="《聖光教教主》/《聖光教教主》.html">《聖光教教主》</a></li>\
 		<li>《燕侍》</li>\
 		<li>《永夜暗夜》</li>\
         <p style="text-align: center;">［收藏］</p><hr>\
