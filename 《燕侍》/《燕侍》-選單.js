aa3ed2ab26967bf550421a1f0468c78a463b8ee6
swallow-234 <s0955502379@gmail.com>
2025-06-06 21:09:04 +0800

    Add files via upload

---
+++ 《燕侍》/《燕侍》-選單.js
@@ -0,0 +1,15 @@
+AA ='<a href="../公佈欄.html">首頁</a>\
+  	<ul >\
+		<p style="text-align: center;">《冥亡伊甸》</p><hr>\
+        <li>《虛擬人生》</li>\
+		<li>初始</li>\
+        <li>初始-戰國時代</li>\
+        <li>《青丘之燕》</li>\
+        <li>-好人</li>\
+        <li>-偷竊</li>\
+		<li>《遺跡先遣隊》</li>\
+        <p style="text-align: center;">［休閒日常］</p><hr>\
+	  </ul>\
+<ul>\
+    </nav>';
+document.write(AA);
