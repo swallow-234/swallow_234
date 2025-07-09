3790e806a33b4d1429b584d5a5799e5127b40efc
swallow-234 <s0955502379@gmail.com>
2025-07-09 22:48:29 +0800

    Add files via upload

---
+++ ~青雲間之飛燕~/公佈欄-選單.js
@@ -0,0 +1,89 @@
+AA ='<a href="https://swallow-234.github.io/swallow_234/%E5%85%AC%E4%BD%88%E6%AC%84">首頁</a>\
+  	<ul >\
+		<p style="text-align: center;">《青丘之燕》</p><hr>\
+        <li><a href="https://swallow-234.github.io/swallow_234/%E3%80%8A%E8%81%96%E5%85%89%E6%95%99%E6%95%99%E4%B8%BB%E3%80%8B/%E3%80%8A%E8%81%96%E5%85%89%E6%95%99%E6%95%99%E4%B8%BB%E3%80%8B">《聖光教教主》</a></li>\
+		<li><a href="https://swallow-234.github.io/swallow_234/%E3%80%8A%E7%87%95%E4%BE%8D%E3%80%8B/%E3%80%8A%E7%87%95%E4%BE%8D%E3%80%8B">《燕侍》</a></li>\
+		<li><a href="https://swallow-234.github.io/swallow_234/%E3%80%8A%E6%B0%B8%E5%A4%9C%E6%9A%97%E5%A4%9C%E3%80%8B/%E3%80%8A%E6%B0%B8%E5%A4%9C%E6%9A%97%E5%A4%9C%E3%80%8B.html">《永夜暗夜》</a></li>\
+        <li><a href="https://swallow-234.github.io/swallow_234/%E3%80%8A%E9%9D%92%E4%B8%98%E8%8C%B6%E9%A4%A8%E3%80%8B/%E3%80%8A%E9%9D%92%E4%B8%98%E8%8C%B6%E9%A4%A8%E3%80%8B.html">《青丘茶館》</a></li>\
+       <p style="text-align: center;">［收藏］</p><hr>\
+        <li><a href="https://swallow-234.github.io/swallow_234/%EF%BC%BB%E6%94%B6%E8%97%8F%EF%BC%BD/POE%E6%B5%81%E4%BA%A1%E6%9A%97%E9%81%93">POE流亡暗道</a></li>\
+        <li><a href="https://swallow-234.github.io/swallow_234/%EF%BC%BB%E6%94%B6%E8%97%8F%EF%BC%BD/FGO.html">Fate/Grand Order</a></li>\
+		<li>pinterest-1</li>\
+		<li>pinterest-2</li>\
+        <li>這是什麼酷酷的東西</li>\
+        <li>休閒牧場</li>\
+        <li>ACG同好會</li>\
+        <li>體育運動</li>\
+        <li>棋類技藝</li>\
+        <li>Vtuber/Vup收藏</li>\
+        <li>台灣[Taiwan]</li>\
+        <li>吹哨者</li>\
+        <li>生活日常</li>\
+        <li>裝扮造型風格</li>\
+        <li>blender</li>\
+        <li>繪圖</li>\
+        <li>劇本</li>\
+        <li>影片/攝影編輯</li>\
+        <li>人生哲學</li>\
+        <li>社會學</li>\
+        <li>開創者</li>\
+        <li>戀愛研究院</li>\
+        <li>法律相關</li>\
+        <li>食譜</li>\
+        <li>醫學</li>\
+        <li>健身|養身|武術</li>\
+        <li>樂理知識</li>\
+        <li>音樂收藏</li>\
+        <li>舞蹈</li>\
+        <li>科學</li>\
+        <li>科技</li>\
+        <li>軍武</li>\
+        <li>工匠技藝</li>\
+        <li>語言研究院</li>\
+        <li><a href="https://swallow-234.github.io/swallow_234/%EF%BC%BB%E6%94%B6%E8%97%8F%EF%BC%BD/%E7%A8%8B%E5%BC%8F%E8%AA%9E%E8%A8%80.html">程式語言</a></li>\
+        <li>Unity遊戲引擎</li>\
+        <li>Google相關</li>\
+        <li>Windows</li>\
+        <li>軟體硬體應用</li>\
+        <li>建築學</li>\
+        <li>風水|玄學|靈異</li>\
+        <li>生物學</li>\
+        <li>泛普教育</li>\
+        <li>防災手冊</li>\
+        <li>防詐騙宣導</li>\
+        <li>AI相關</li>\
+        <h3><a href="https://swallow-234.github.io/swallow_234/%EF%BC%BB%E6%94%B6%E8%97%8F%EF%BC%BD/%E7%B3%BB%E5%88%97%E4%B8%BB%E9%A1%8C.html">系列主題</a></h3><hr>\
+        <li>柯文哲-先行者</li>\
+        <li>黃國昌-通識課</li>\
+        <li>陳之漢-義士</li>\
+        <li>陳椒華-循道者</li>\
+        <li><a href="https://swallow-234.github.io/swallow_234/%EF%BC%BB%E6%94%B6%E8%97%8F%EF%BC%BD/%E5%8F%B0%E7%81%A3%E6%B0%91%E7%9C%BE%E9%BB%A8%E5%B0%88%E5%8D%80.html">台灣民眾黨專區</a></li>\
+        <h3>工具收藏</h3><hr>\
+<li><a href="https://www.blender.org/">blender</a></li>\
+<li><a href="https://krita.org/zh-tw/download_zh-tw/krita-desktop_zh-tw/">Krita</a></li>\
+<li><a href="https://obsidian.md/">Obsidian</a></li>\
+<li><a href="https://zh-tw.libreoffice.org/">LibreOffice</a></li>\
+<li><a href="https://visipics.info/download/">VisiPics</a></li>\
+<li><a href="https://www.sublimetext.com/">Sublime Text</a></li>\
+<li><a href="https://www.python.org/">Python</a></li>\
+<li><a href="https://code.visualstudio.com/">visual studio code</a></li>\
+<li><a href="https://obsproject.com/">OBS Studio</a></li>\
+<li><a href="https://unity.com/">Unity</a></li>\
+<li><a href="https://www.drawio.com/">drawio</a></li>\
+<li><a href="https://www.xmind.net/">xmind</a></li>\
+<li><a href="https://discord.com/channels/@me">Discord</a></li>\
+<li><a href="https://potplayer.daum.net/?lang=zh_TW">PotPlayer</a></li>\
+<li><a href="https://home.gamer.com.tw/profile/index.php?&owner=hbl917070">深海異音</a></li>\
+<li><a href="https://www.geogebra.org/graphing?lang=zh-TW">繪圖計算機 - GeoGebra</a></li>\
+<li><a href="https://efotinis.neocities.org/deskpins/">DeskPins</a></li>\
+<li><a href="https://www.7-zip.org/">7-Zip</a></li>\
+	  </ul>\
+<ul>\
+    </nav>';
+document.write(AA);
+
+
+
+
+
+
